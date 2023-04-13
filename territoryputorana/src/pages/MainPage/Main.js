1468159e0b7153983ef44b2cc6a9d98599566740
vladpon <2874787@gmail.com>
2023-04-13 16:58:28 +0700

    lst

--- territoryputorana/src/pages/MainPage/Main.js
+++ territoryputorana/src/pages/MainPage/Main.js
@@ -11,13 +11,82 @@ const textWord = {
   word: 'путорана'
 }
 
+const tours = [
+  {
+    id: '001',
+    title: 'Затерянный мир Плато Путорана',
+    season: 'лето',
+    time: '8 дней / 7 ночей',
+    price: 'от 230 000',
+    bigImg: '',
+    smallImg: './img/mp_lostput.jpg',
+    optImg: '',
+    tourPhoto: []
+  },
+  {
+    id: '002',
+    title: 'Тур выходного дня',
+    season: 'лето',
+    time: 'пт-вс',
+    price: 'от 35 000',
+    bigImg: '',
+    smallImg: './img/mp_weekend.jpg',
+    optImg: '',
+    tourPhoto: []
+  },
+  {
+    id: '003',
+    title: 'Палаточный лагерь',
+    season: 'лето',
+    time: 'от 1 дня',
+    price: 'от 10 000',
+    bigImg: '',
+    smallImg: './img/mp_camp.jpg',
+    optImg: '',
+    tourPhoto: []
+  },
+  {
+    id: '004',
+    title: 'Ски-тур',
+    season: 'весна',
+    time: '7 дней / 6 ночей',
+    price: 'от 160 000',
+    bigImg: '',
+    smallImg: './img/mp_skitour.jpg',
+    optImg: '',
+    tourPhoto: []
+  },
+  {
+    id: '005',
+    title: 'Трейлраннинг кемп',
+    season: 'лето',
+    time: '8 дней / 7 ночей',
+    price: 'от 230 000',
+    bigImg: '',
+    smallImg: './img/mp_trailrunning.jpg',
+    optImg: '',
+    tourPhoto: []
+  },  
+  {
+    id: '006',
+    title: 'Вертолетные экскурсии',
+    season: 'всесезонный',
+    time: '',
+    price: 'от 200 000* руб.',
+    bigImg: '',
+    smallImg: './img/mp_heli.jpg',
+    optImg: '',
+    tourPhoto: []
+  }
+]
+
 const Main = () => {
   return (
     <div className='main-page'>
       <Header headerBackgroundColor = 'rgba(100, 100, 100, 0.5)'/>
       <Cover />
       <TextWord text = {textWord.text} word = {textWord.word} />
-      <CardsBlock />
+      <CardsBlock tours = {tours}/>
       <Footer />
     </div>
   )
--- territoryputorana/src/pages/MainPage/components/CardsBlock.js
+++ territoryputorana/src/pages/MainPage/components/CardsBlock.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 
 
-const CardsBlock = () => {
+const CardsBlock = (props) => {
+    const { tours } = props;
   return (
     <div class="cards-block">
             <div class="cards-block__title">
@@ -10,96 +11,31 @@ const CardsBlock = () => {
                 <p>Готовые программы для вашего путешествия и отдыха</p>
             </div>
             <div class="cards-block__card-container card-container">
-                <div class="card-container__card">
-                    <div class="card-container__bg">
-                        <img src='./img/mp_lostput.jpg'></img>
-                    </div>
-                    <div class="card-container__text-container">
-                        <div class="card-container__top-text">
-                            <div class="card-container__season">лето</div>
-                            <div class="card-container__title">Затерянный мир Плато Путорана</div>
+
+                {tours ? (
+                    tours.map( (tour, index) => {
+                        if (index < 6) return (
+                        <div class="card-container__card">
+                            <div class="card-container__bg">
+                                <img src={tour.smallImg}></img>
+                            </div>
+                            <div class="card-container__text-container">
+                                <div class="card-container__top-text">
+                                    <div class="card-container__season">{tour.season}</div>
+                                    <div class="card-container__title">{tour.title}</div>
+                                </div>
+                                <div class="card-container__bottom-text">
+                                    <div class="card-container__days">{tour.time}</div>
+                                    <div class="card-container__price">{tour.price}</div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="card-container__bottom-text">
-                            <div class="card-container__days">8 дней/7 ночей</div>
-                            <div class="card-container__price">от 230 000</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card-container__card">
-                    <div class="card-container__bg">
-                        <img src='./img/mp_lostput.jpg'></img>
-                    </div>
-                    <div class="card-container__text-container">
-                        <div class="card-container__top-text">
-                            <div class="card-container__season">лето</div>
-                            <div class="card-container__title">Затерянный мир Плато Путорана</div>
-                        </div>
-                        <div class="card-container__bottom-text">
-                            <div class="card-container__days">8 дней/7 ночей</div>
-                            <div class="card-container__price">от 230 000</div>
-                        </div>
-                    </div>
-                </div>                 
-                <div class="card-container__card">
-                    <div class="card-container__bg">
-                        <img src='./img/mp_lostput.jpg'></img>
-                    </div>
-                    <div class="card-container__text-container">
-                        <div class="card-container__top-text">
-                            <div class="card-container__season">лето</div>
-                            <div class="card-container__title">Затерянный мир Плато Путорана</div>
-                        </div>
-                        <div class="card-container__bottom-text">
-                            <div class="card-container__days">8 дней/7 ночей</div>
-                            <div class="card-container__price">от 230 000</div>
-                        </div>
-                    </div>
-                </div> 
-                <div class="card-container__card">
-                    <div class="card-container__bg">
-                        <img src='./img/mp_lostput.jpg'></img>
-                    </div>
-                    <div class="card-container__text-container">
-                        <div class="card-container__top-text">
-                            <div class="card-container__season">лето</div>
-                            <div class="card-container__title">Затерянный мир Плато Путорана</div>
-                        </div>
-                        <div class="card-container__bottom-text">
-                            <div class="card-container__days">8 дней/7 ночей</div>
-                            <div class="card-container__price">от 230 000</div>
-                        </div>
-                    </div>
-                </div> 
-                <div class="card-container__card">
-                    <div class="card-container__bg">
-                        <img src='./img/mp_lostput.jpg'></img>
-                    </div>
-                    <div class="card-container__text-container">
-                        <div class="card-container__top-text">
-                            <div class="card-container__season">лето</div>
-                            <div class="card-container__title">Затерянный мир Плато Путорана</div>
-                        </div>
-                        <div class="card-container__bottom-text">
-                            <div class="card-container__days">8 дней/7 ночей</div>
-                            <div class="card-container__price">от 230 000</div>
-                        </div>
-                    </div>
-                </div> 
-                <div class="card-container__card">
-                    <div class="card-container__bg">
-                        <img src='./img/mp_lostput.jpg'></img>
-                    </div>
-                    <div class="card-container__text-container">
-                        <div class="card-container__top-text">
-                            <div class="card-container__season">лето</div>
-                            <div class="card-container__title">Затерянный мир Плато Путорана</div>
-                        </div>
-                        <div class="card-container__bottom-text">
-                            <div class="card-container__days">8 дней/7 ночей</div>
-                            <div class="card-container__price">от 230 000</div>
-                        </div>
-                    </div>
-                </div> 
+                        )
+                    })
+                ) :
+                <h1>No Tours found</h1>}
+                
+                
             </div>
             <div class="cards-block__link"><a href="./tours.html"> Перейти в каталог туров </a></div>
         </div>
