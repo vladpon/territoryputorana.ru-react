35d16fd4a89fa01e790b2425ed8a3401dfeea9ab
vladpon <2874787@gmail.com>
2023-04-16 19:35:47 +0700

    lst

--- territoryputorana/src/pages/TourPage/TourPage.js
+++ territoryputorana/src/pages/TourPage/TourPage.js
@@ -3,12 +3,16 @@ import React from 'react'
 import './styles.scss'
 import TourPageCover from './components/TourPageCover'
 import TourPageAbout from './components/TourPageAbout'
+import TourPageAux from './components/TourPageAux'
+import TourPageProgram from './components/TourPageProgram'
 
 const TourPage = () => {
   return (
     <main className='tour-page'>
         <TourPageCover />
         <TourPageAbout />
+        <TourPageAux />
+        <TourPageProgram />
     </main>
   )
 }
