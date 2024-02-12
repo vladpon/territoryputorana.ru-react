140d7023302e6421a23287ab7ef26536c8cece47
vlad <2874787@gmail.com>
2024-02-13 01:14:13 +0700

    get tour by id API

--- territoryputorana/api/tours_api.php
+++ territoryputorana/api/tours_api.php
@@ -18,35 +18,177 @@ global $answer;
 // var_dump($_SESSION);
 // var_dump($_COOKIE);
 
+function descriptionsFilter($item) {
+    return $item;
+}
 
+
+//////// GET TOUR BY ID ///////////////
 
 if(isset($_GET['gettourbyid']))
 {
     if(isset($_GET['id']) && $_GET['id'] != NULL)
     {
         $tourId = $_GET['id'];
-        $sqlStr = 'SELECT * FROM tours WHERE tour_id = :id;';
+        $tour = [];
+
+        $sqlMain =          'SELECT 
+                                title AS title,
+                                price AS price,
+                                var_price AS varPrice,
+                                season AS season,
+                                time AS time,
+                                reference AS reference,
+                                var_reference AS varReference,
+                                included AS included,
+                                var_included AS varIncluded,
+                                big_img AS bigImg,
+                                small_img AS smallImg,
+                                href AS href,
+                                details AS details,
+                                var_details AS varDetails,
+                                details_title AS detailsTitle,
+                                var_detailstitle AS varDetailsTitle,
+                                about_title AS aboutTitle
+                                FROM tours WHERE tour_id = :id;';
+
+        $sqlProgramTitles = 'SELECT                                
+                                program_title AS programTitle,
+                                program_subtitle AS programSubtitle,
+                                program_preface AS programPreface                                
+                                FROM tours WHERE tour_id = :id;';
+
+        $sqlDescriptions =  'SELECT 
+                                id AS descriptionIndex,
+                                paragraph AS paragraph
+                                FROM descriptions WHERE tour_id = :id;';
+        
+        $sqlAbouts =        'SELECT
+                                id AS aboutIndex,
+                                paragraph AS paragraph
+                                FROM abouts WHERE tour_id = :id;';
+
+        $sqlPhotos =        'SELECT
+                                -- id AS photoId,
+                                path AS path,
+                                alt AS alt
+                                FROM tours_photos WHERE tour_id = :id;';
+
+        $sqlPrograms =      'SELECT
+                                id AS programIndex,
+                                day_img AS dayImg,
+                                day_title AS dayTitle
+                                FROM programs_days WHERE tour_id = :id;';
+
+        $sqlDaysDescriptions = 'SELECT
+                                id AS paragraphId,
+                                day_id AS dayId,
+                                paragraph AS paragraph
+                                FROM days_descriptions WHERE tour_id = :id';
 
 
+
+        /// PDO ///
+        
         try {
+
+            ///// PDO construct /////
             $pdo = new PDO(
                 'mysql:host=' . $DBHOST . ':3306;dbname=' . $DBNAME,
                 $DBNAME,
                 $DBPASS
             );
+
         
-            $stmt = $pdo->prepare($sqlStr);
+            //// MAIN TABLE FETCH ////
+
+            $stmt = $pdo->prepare($sqlMain);
             $state = $stmt->execute(array('id' => $tourId));
             $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            $tour = $res[0];
+ 
+
+            //// DESCRIPTION TABLE FETCH ////
+
+            $stmt = $pdo->prepare($sqlDescriptions, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+            $state = $stmt->execute(array('id' => $tourId));
+
+            
+            while($res = $stmt->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
+                $tour['description'][$res[0]] = $res[1];
+            }
+
+            //// ABOUTS TABLE FETCH ////
+
+            $stmt = $pdo->prepare($sqlAbouts, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+            $state = $stmt->execute(array('id' => $tourId));
+            
+            while($res = $stmt->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
+                $tour['about'][$res[0]] = $res[1];
+            }
+
+            //// TOURS_PHOTOS TABLE FETCH 
+
+            $stmt = $pdo->prepare($sqlPhotos, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+            $state = $stmt->execute(array('id' => $tourId));
+            
+            $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $tour['tourPhoto'] = $res;
+            
+
+            //// MAIN TABLE FOR TOUR PROGRAM FETCH ////
+
+            $stmt = $pdo->prepare($sqlProgramTitles);
+            $state = $stmt->execute(array('id' => $tourId));
+            $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            $tour['tourProgram'] = $res[0];
+
+
+            /// PROGRAMS_DAYS TABLES FETCH ////
+            $stmt = $pdo->prepare($sqlPrograms, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+            $state = $stmt->execute(array('id' => $tourId));
+
+            while($res = $stmt->fetch(PDO::FETCH_ASSOC)){
+                $tour['tourProgram']['days'][$res['programIndex']]['dayImg'] = $res['dayImg'];
+                $tour['tourProgram']['days'][$res['programIndex']]['dayTitle'] = $res['dayTitle'];
+                
+            }
+
+
+            /// DAYS_DESCRITPIONS TABLE FETCH ////
+
+            $stmt = $pdo->prepare($sqlDaysDescriptions, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+            $state = $stmt->execute(array('id' => $tourId));
+
+            while($res = $stmt->fetch(PDO::FETCH_ASSOC)){
+                $tour['tourProgram']['days'][$res['dayId']]['dayDesc'][$res['paragraphId']] = $res['paragraph'];                
+            }
+
+
+
+
+
             }
             catch (Exception $e) {
                 echo $e->getMessage();
             }
 
-        echo json_encode($res, JSON_UNESCAPED_UNICODE);
+        echo json_encode($tour, JSON_UNESCAPED_UNICODE);
     }
-    else echo 'no tour ID';
+    else $answer['gettourbyid'] = 'failed: no tour id';
 }
+
+
+
+
+
+
+
+
+
+
 
 elseif(isset($_GET['gettourbytourid']))
 {
@@ -215,3 +357,4 @@ else
 
 
 
+// echo json_encode($answer);
